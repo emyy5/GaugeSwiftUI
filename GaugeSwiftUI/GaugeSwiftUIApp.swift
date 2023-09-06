f2a3c561820d91c82b84f15c0beae63ca15bfb63
Github <emyy5>
2023-09-06 03:22:16 +0300

    Initial Commit

---
+++ GaugeSwiftUI/GaugeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeSwiftUIApp.swift
+//  GaugeSwiftUI
+//
+//  Created by Eman Khaled on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct GaugeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
